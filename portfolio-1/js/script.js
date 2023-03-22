c02baf62b509dee9f3b2885ef60c80c4252a48a5
msedatkartal <mmsedatkartal@gmail.com>
2023-03-22 11:49:03 +0300

    .

---
+++ portfolio-1/js/script.js
@@ -0,0 +1,7 @@
+// ================= typing animation =========================
+var tyed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
